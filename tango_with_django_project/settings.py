8d9d4c04fc264c54f2977477850989cbcc5345ea
praharsh05 <praharsh.dubey05@gmail.com>
2023-01-26 21:19:14 +0000

    comment for 3.3 app creation

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rango',
+    'rango',#added as we created an app rango
 ]
 
 MIDDLEWARE = [

3a7bb165be3a2fafee8b1f4b5dcccebe4d7d4083
praharsh05 <praharsh.dubey05@gmail.com>
2023-02-02 17:41:07 +0000

    6.2 showing categories on index page

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render  # to render pages
 from django.http import HttpResponse  # to send httpResponse to requests
+from rango.models import Category # to import the categories set in the model
 
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     # return HttpResponse("Rango says Hey there partner!")
-    # return HttpResponse("Rango says hey there partner!\n Rango says here is the index page \n  <a href='/rango/about/'>About</a>")
+    # return HttpResponse("Rango says hey there partner!\n Rango says here is the 
+    # index page \n  <a href='/rango/about/'>About</a>")
+
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only -- or all if less than 5.
+    # Place the list in our context_dict dictionary (with our boldmessage!)
+    # that will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    #render the respsonse and send it back
     return render(request, 'rango/index.html', context=context_dict)
 
 

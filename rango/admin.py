f4a9279bf8f5e37c0c4cb2c9fe9eb5a391123b15
praharsh05 <praharsh.dubey05@gmail.com>
2023-02-02 13:19:12 +0000

    5.6 admin interface

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
 # Register your models here.
+admin.site.register(Category) # to register the Category and the Page model to be viewed in the admin page 
+admin.site.register(Page)

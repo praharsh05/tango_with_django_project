63f90c5ecca075944eec8e81de9b42c512cc4822
praharsh05 <praharsh.dubey05@gmail.com>
2023-02-02 16:45:38 +0000

    ch 5 exercise

--- rango/admin.py
+++ rango/admin.py
@@ -3,4 +3,10 @@ from rango.models import Category, Page
 
 # Register your models here.
 admin.site.register(Category) # to register the Category and the Page model to be viewed in the admin page 
-admin.site.register(Page)
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Page, PageAdmin)
